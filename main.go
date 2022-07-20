a186339bb3ddf9ce5895cb84bafc7e8ba1664fe6
Vyacheslav Starostin <vyacheslav.starostin@sap.com>
2022-07-20 23:07:54 +0600

    linttest branch to main (#2)
    * use GPP jliempt/piperBranch
    
    * set runLinter to true
    
    * disable errcheck linter
    
    * change sonar project name back
    
    * upload TEST-go.xml to Sonar
    
    * Update sonar-project.properties
    
    * enable errcheck linter
    
    * add junit report
    
    * verbose: true
    
    * disable errcheck linter
    
    * Update config.yml
    
    * Update sonar-project.properties
    
    * Update sonar-project.properties
    
    * Update config.yml
    
    * Update sonar-project.properties
    
    * Update main.go
    
    * Update main.go
    
    * Update main.go
    
    * Update main.go
    
    * Update users.go
    
    * Update users.go
    
    * Update config.yml
    
    * Update .golangci.yaml
    
    * Update .golangci.yaml
    
    * Update .golangci.yaml
    
    * Update .golangci.yaml
    
    Co-authored-by: Jordi van Liempt <jordi.van.liempt@sap.com>

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 		port = "8080"
 		log.Printf("Defaulting to port %s", port)
 	}
-
+	
 	users := types.DefaultUsers()
 
 	r := gin.Default()
@@ -24,6 +24,7 @@ func main() {
 			"message": "alive",
 		})
 	})
+	
 	r.GET("/users", func(c *gin.Context) {
 		c.JSON(200, users)
 	})
--- pkg/types/users.go
+++ pkg/types/users.go
@@ -11,5 +11,242 @@ func DefaultUsers() []User {
 		{Name: "User3"},
 		{Name: "User4"},
 	}
+	users = []User{
+		{Name: "User1"},
+		{Name: "User2"},
+		{Name: "User3"},
+		{Name: "User4"},
+	}
+users = []User{
+		{Name: "User1"},
+		{Name: "User2"},
+		{Name: "User3"},
+		{Name: "User4"},
+	}
+users = []User{
+		{Name: "User1"},
+		{Name: "User2"},
+		{Name: "User3"},
+		{Name: "User4"},
+	}
+users = []User{
+		{Name: "User1"},
+		{Name: "User2"},
+		{Name: "User3"},
+		{Name: "User4"},
+	}
+users = []User{
+		{Name: "User1"},
+		{Name: "User2"},
+		{Name: "User3"},
+		{Name: "User4"},
+	}
+users = []User{
+		{Name: "User1"},
+		{Name: "User2"},
+		{Name: "User3"},
+		{Name: "User4"},
+	}
+users = []User{
+		{Name: "User1"},
+		{Name: "User2"},
+		{Name: "User3"},
+		{Name: "User4"},
+	}
+users = []User{
+		{Name: "User1"},
+		{Name: "User2"},
+		{Name: "User3"},
+		{Name: "User4"},
+	}
+users = []User{
+		{Name: "User1"},
+		{Name: "User2"},
+		{Name: "User3"},
+		{Name: "User4"},
+	}
+users = []User{
+		{Name: "User1"},
+		{Name: "User2"},
+		{Name: "User3"},
+		{Name: "User4"},
+	}
+users = []User{
+		{Name: "User1"},
+		{Name: "User2"},
+		{Name: "User3"},
+		{Name: "User4"},
+	}
+users = []User{
+		{Name: "User1"},
+		{Name: "User2"},
+		{Name: "User3"},
+		{Name: "User4"},
+	}
+users = []User{
+		{Name: "User1"},
+		{Name: "User2"},
+		{Name: "User3"},
+		{Name: "User4"},
+	}
+users = []User{
+		{Name: "User1"},
+		{Name: "User2"},
+		{Name: "User3"},
+		{Name: "User4"},
+	}
+users = []User{
+		{Name: "User1"},
+		{Name: "User2"},
+		{Name: "User3"},
+		{Name: "User4"},
+	}
+users = []User{
+		{Name: "User1"},
+		{Name: "User2"},
+		{Name: "User3"},
+		{Name: "User4"},
+	}
+users = []User{
+		{Name: "User1"},
+		{Name: "User2"},
+		{Name: "User3"},
+		{Name: "User4"},
+	}
+users = []User{
+		{Name: "User1"},
+		{Name: "User2"},
+		{Name: "User3"},
+		{Name: "User4"},
+	}
+users = []User{
+		{Name: "User1"},
+		{Name: "User2"},
+		{Name: "User3"},
+		{Name: "User4"},
+	}
+users = []User{
+		{Name: "User1"},
+		{Name: "User2"},
+		{Name: "User3"},
+		{Name: "User4"},
+	}
+users = []User{
+		{Name: "User1"},
+		{Name: "User2"},
+		{Name: "User3"},
+		{Name: "User4"},
+	}
+users = []User{
+		{Name: "User1"},
+		{Name: "User2"},
+		{Name: "User3"},
+		{Name: "User4"},
+	}
+users = []User{
+		{Name: "User1"},
+		{Name: "User2"},
+		{Name: "User3"},
+		{Name: "User4"},
+	}
+users = []User{
+		{Name: "User1"},
+		{Name: "User2"},
+		{Name: "User3"},
+		{Name: "User4"},
+	}
+users = []User{
+		{Name: "User1"},
+		{Name: "User2"},
+		{Name: "User3"},
+		{Name: "User4"},
+	}
+users = []User{
+		{Name: "User1"},
+		{Name: "User2"},
+		{Name: "User3"},
+		{Name: "User4"},
+	}
+users = []User{
+		{Name: "User1"},
+		{Name: "User2"},
+		{Name: "User3"},
+		{Name: "User4"},
+	}
+users = []User{
+		{Name: "User1"},
+		{Name: "User2"},
+		{Name: "User3"},
+		{Name: "User4"},
+	}
+users = []User{
+		{Name: "User1"},
+		{Name: "User2"},
+		{Name: "User3"},
+		{Name: "User4"},
+	}
+users = []User{
+		{Name: "User1"},
+		{Name: "User2"},
+		{Name: "User3"},
+		{Name: "User4"},
+	}
+users = []User{
+		{Name: "User1"},
+		{Name: "User2"},
+		{Name: "User3"},
+		{Name: "User4"},
+	}
+users = []User{
+		{Name: "User1"},
+		{Name: "User2"},
+		{Name: "User3"},
+		{Name: "User4"},
+	}
+users = []User{
+		{Name: "User1"},
+		{Name: "User2"},
+		{Name: "User3"},
+		{Name: "User4"},
+	}
+users = []User{
+		{Name: "User1"},
+		{Name: "User2"},
+		{Name: "User3"},
+		{Name: "User4"},
+	}
+users = []User{
+		{Name: "User1"},
+		{Name: "User2"},
+		{Name: "User3"},
+		{Name: "User4"},
+	}
+users = []User{
+		{Name: "User1"},
+		{Name: "User2"},
+		{Name: "User3"},
+		{Name: "User4"},
+	}
+users = []User{
+		{Name: "User1"},
+		{Name: "User2"},
+		{Name: "User3"},
+		{Name: "User4"},
+	}
+users = []User{
+		{Name: "User1"},
+		{Name: "User2"},
+		{Name: "User3"},
+		{Name: "User4"},
+	}
+users = []User{
+		{Name: "User1"},
+		{Name: "User2"},
+		{Name: "User3"},
+		{Name: "User4"},
+	}
+
 	return users
 }
+
+
